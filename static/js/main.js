8aba2310c20890069e30452cc01fce5fc5dbd117
kuailianl88 <calorimeta@gmail.com>
2025-08-22 13:31:12 +0800

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+﻿// 获取具有类名 "down" 的链接元素
+var downLinks = document.querySelectorAll(".down");
+
+// 为每个链接添加点击事件监听器
+downLinks.forEach(function(link) {javascript:;
+    link.addEventListener("click", function(event) {
+        // 阻止默认的链接点击行为
+        event.preventDefault();
+
+        // 执行跳转操作，替换下面的URL为您想要跳转的URL
+        window.location.href = "https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_zce10df696d66b3179d810519_f107738cb692e4610_d20250818_m095512_c005_v0501002_t0000_u01755510912324";
+    });
+});
